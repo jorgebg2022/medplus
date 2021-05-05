d4aea4d2ff229a2997d3f198216e39052579476f
jorgebg2022 <jorgebg2022@gmail.com>
2021-05-05 19:38:28 -0300

    deploy 008

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $config['base_url'] = 'http://127.0.0.1:80/medplusplus/';
 
-$config['base_url'] = 'https://medplus-app.herokuapp.com/medplus-app';
+$config['base_url'] = 'https://medplus-app.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
