b566610a22e19c427168bc8ecc92046540427a56
jorgebg2022 <jorgebg2022@gmail.com>
2021-05-05 21:28:13 -0300

    deploy

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+// $config['base_url'] = 'http://127.0.0.1:80/medplusplus/';
+
 $config['base_url'] = 'https://medplusnewapp.herokuapp.com/';
 
-// $config['base_url'] = 'https://medplus-app.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -37,7 +38,7 @@ $config['base_url'] = 'https://medplusnewapp.herokuapp.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = APPPATH.'../../../index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
