4d0b6422ac55384771eebcc2e47d1da7e992f921
jorgebg2022 <jorgebg2022@gmail.com>
2021-05-05 21:10:54 -0300

    deploy

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://0.0.0.0:80/medplus';
+$config['base_url'] = 'https://medplusnewapp.herokuapp.com/';
 
 // $config['base_url'] = 'https://medplus-app.herokuapp.com/';
 
